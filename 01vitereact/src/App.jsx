895019474c1f6c1874cca7c160f78ea7bf8398be
ashish patil <patil24ashish@gmail.com>
2026-03-29 17:44:44 +0530

    added new component Title

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Title from "./myfirst"
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hello react</h1>
+      <Title />
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/myfirst.jsx
@@ -0,0 +1,7 @@
+function Title(){
+    return (
+        <h4>This is Title for the banner</h4>
+    )
+}
+
+export default Title
